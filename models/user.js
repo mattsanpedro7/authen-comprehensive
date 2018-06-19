a52efc07e986b544492172485084768b88daf31f
mattNat <mattsanpedro76@gmail.com>
2018-06-18 22:50:21 -0700

    add bcrypt salt and hash to saved passwords

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
 // schema particular fields model has
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // define our model
 // email should be unique
@@ -9,6 +9,28 @@ const userSchema = new Schema({
   // lowercase: first turn to powercase when email is saved
   email: { type: String, unique: true, lowercase: true }, 
   password: String
+});
+
+// on save hook, encrypt password
+// before saving a model, run this function
+userSchema.pre('save', function(next) {
+  // access to user model
+  const user = this;
+
+  // generate a salt, then run callback
+  bcrypt.genSalt(12, function(err, salt) {
+    if (err) { return next(err); }
+
+    // hash (encrypt) our password using the salt (random gen string of char)
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      // overwrite plain text pas with encrypted
+      user.password = hash;
+      // go ahead save the model
+      next();
+    });
+  })
 });
 
 // create model class - represents all users
