28ebc477c0c92fed63b39ddbf7d1e11aa888a135
mattNat <mattsanpedro76@gmail.com>
2018-06-18 18:58:21 -0700

    create router and hook up to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const router = require('./router');
 
 // create instance of express
 const app = express();
@@ -14,6 +15,7 @@ const app = express();
 // morgan is a logging framework
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 // express talking to outside
---
+++ router.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+  // function that handles routes
+  // next: mostly for error handling
+  app.get('/', function(req, res, next) {
+    res.send(['ps4', 'pc', 'asus']);
+  });
+
+
+}
