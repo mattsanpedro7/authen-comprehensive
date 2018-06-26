32d806fd11fa99000508421965a796b6e7d0acab
mattNat <mattsanpedro76@gmail.com>
2018-06-25 23:48:25 -0700

    add jwtOptions and jwtLogin

--- services/passport.js
+++ services/passport.js
@@ -8,7 +8,13 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // setup options for JWT strategy
-const jwtOptions = {};
+// 1. expecting JwtStrategy is going to get access to jwt off req
+// 2. specfically tell strategy where to look on req to find key
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    // need to tell secret to decode token
+    secretOrKey: config.secret
+};
 
 // create JWT strategy
 // payload is decoded jwt token
@@ -17,6 +23,15 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
     // see if the user ID in the payload exists in our databse
     // if it does, call 'done' with that user
     // otherwise, call done without a user object
+    User.findById(payload.sub, function(err, user) {
+        if (err) {return done(err, false);}
+        
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
 });
 
 // tell passport to use this strategy
