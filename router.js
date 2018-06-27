5da227f7e5f5f6d5845af3e0efd87051c69d08dd
mattNat <mattsanpedro76@gmail.com>
2018-06-26 18:29:29 -0700

    create object "requireAuth" to handle imcoming req and route handler

--- router.js
+++ router.js
@@ -1,6 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// object occurs in middle between incoming req and route handler
+// session: false => when user is authen, don't create a authen cookie
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ hi: 'there' })
+  });
   // function that handles routes
   // next: mostly for error handling
   app.post('/signup', Authentication.signup);
